b213d0b024efc9dad169ff0cce35a3826487cb67
aakbar24 <makbar24@gmail.com>
2016-01-12 19:00:35 -0500

    initial

---
+++ TestSelenium_driver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using seleniumTest;
+
+namespace TestSelenium_driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            login test = new login();
+            test.browseGoogleSite();
+            Console.Read();
+        }
+    }
+}
---
+++ seleniumTest/login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace seleniumTest
+{
+    public class login
+    {
+        public void browseGoogleSite()
+        {
+
+            IWebDriver _driver = new ChromeDriver();
+            //IWebDriver _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl("https://www.google.ca");
+            //lst-ib
+            //btnG
+
+
+              IWebElement text =   _driver.FindElement(By.Id("lst-ib"));//.SendKeys("serach for test");
+              text.SendKeys("test for QA");
+
+              IWebElement sub = _driver.FindElement(By.Name("btnG"));
+             // text.Submit();
+              sub.Click();
+        }
+
+       
+        
+
+    }
+}
